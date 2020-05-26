97ffd07f5fc9c834755876fb8a7ae3c30cdef43a
choron123 <65941297+choron123@users.noreply.github.com>
2020-05-26 18:39:17 +0800

    update 03
    update 03

---
+++ SRC/COM/合唱/试验/test.java
@@ -0,0 +1,10 @@
+package com.choron.test;
+
+public class test {
+	public static void main(String[]args) {
+		System.out.print("ÄãºÃÔ¶³Ì²Ö¿âgithub");
+		System.out.print("ÄãºÃµÚ¶þ´Î·ÃÎÊ");
+		System.out.print("gihub操作");
+	}
+
+}
--- src/com/choron/test/test.java
+++
@@ -1,9 +0,0 @@
-package com.choron.test;
-
-public class test {
-	public static void main(String[]args) {
-		System.out.print("你好远程仓库github");
-		System.out.print("你好第二次访问");
-	}
-
-}
